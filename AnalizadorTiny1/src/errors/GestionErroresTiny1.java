4108455a38a5f464f556cbd755c3d79e9fdcd46d
pablodll <pablo.daurell@gmail.com>
2021-04-25 11:35:09 +0200

    Modulo errors

---
+++ AnalizadorTiny1/src/errors/GestionErroresTiny1.java
@@ -0,0 +1,15 @@
+package errors;
+
+import alex.UnidadLexica;
+
+public class GestionErroresTiny1 {
+
+	public void errorLexico(int fila, int columna, String lexema) {
+		System.err.println("ERROR fila " + fila + ", columna " + columna + ": Caracter inesperado: " + lexema);
+		System.exit(1);
+	}
+	
+	public void errorSintactico(UnidadLexica ud) {
+		System.err.println("ERROR fila " + ud.fila() + ", columna " + ud.columna() + ": Elemento inesperado: " + ud.lexema());
+	}
+}
